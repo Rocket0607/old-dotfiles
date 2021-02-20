eaaf313f33287f7f8c1c6c1b44d12d87f44e4606
Rocket0607 <konro@utschools.ca>
2021-02-20 08:42:02 -0500

    added alacritty, fish, pacwall, qtile, rofi and starship configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+starship init fish | source
+set fish_greeting
+colorscript --random
+
+alias todo="python3 /home/rohitk/Documents/todo-terminal/main.py"
+alias git-push="python3 /home/rohitk/Scripts/git_add_commit_push.py"
+alias notes="nvim /home/rohitk/vimwiki/index.wiki"
+alias pomodoro="cd /home/rohitk/Applications/github/thomas && npm run start"
+alias doom="~/.emacs.d/bin/doom"
