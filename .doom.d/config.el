c925c03b310d5906af5ea3271ce5626ae72a6b97
Rocket0607 <konro@utschools.ca>
2021-02-22 08:13:08 -0500

    update doom configs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -49,6 +49,8 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Documents/org/")
 
+(setq org-hide-emphasis-markers 5)
+
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
